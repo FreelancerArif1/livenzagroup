160a0d032a5560c32ac2592d04aa5562e7efb16a
Arif Hossain <arifindex22@gmail.com>
2025-12-02 10:14:45 +0600

    Update career.blade.php

--- resources/views/frontend/pages/career.blade.php
+++ resources/views/frontend/pages/career.blade.php
@@ -167,21 +167,11 @@
         @endif
 
         @if ($career?->block_three_title)
-
-
-
-
             <div class="page-banner overlay">
                 <div class="media media-bg">
-                    @if ($career?->block_three_video)
-                        <video class="full-video" autoplay muted loop playsinline>
-                            <source src="{{ $career?->block_three_video }}" type="video/mp4">
-                        </video>
-                    @else
-                        <img src="{{ $career?->block_three_image }}" width="1920" height="520" loading="eager"
-                            alt="Page Banner Image">
-                    @endif
-
+                    <video class="full-video" autoplay muted loop playsinline>
+                        <source src="{{ $career?->block_three_video }}" type="video/mp4">
+                    </video>
                 </div>
                 <div class="page-banner-content">
                     <div class="container">
@@ -192,23 +182,6 @@
                                         <div class="image radius18 height-100" data-aos="fade-right">
                                             <img src="{{ $career?->block_three_image }}" width="1000" height="742"
                                                 loading="lazy" alt="Image">
-                                            @if ($career?->block_three_video)
-                                                <div class="content-absolute">
-                                                    <div
-                                                        class="flex-banner height-inherit d-flex align-items-center justify-content-center">
-                                                        <div class="content-box">
-                                                            <div class="button button--secondary open-video">
-                                                                <svg class="icon-24" viewBox="0 0 12 12" fill="none"
-                                                                    xmlns="http://www.w3.org/2000/svg">
-                                                                    <path
-                                                                        d="M10.858 5.1281C11.5377 5.51041 11.5377 6.48895 10.858 6.87125L2.04588 11.8281C1.37927 12.203 0.555618 11.7213 0.555618 10.9565V1.04285C0.555618 0.278027 1.37928 -0.203685 2.04588 0.171279L10.858 5.1281Z"
-                                                                        fill="currentColor" />
-                                                                </svg>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            @endif
                                         </div>
                                         <div class="video-modal">
                                             <div class="video-modal-inner">
@@ -218,9 +191,6 @@
                                                         <path d="M6 18 18 6M6 6l12 12" />
                                                     </svg>
                                                 </span>
-                                                <video class="video-frame" width="100%" height="100%" controls>
-                                                    <source src="{{ $career?->block_three_video }}" type="video/mp4">
-                                                </video>
                                             </div>
                                         </div>
                                     </modal-video>
@@ -242,12 +212,6 @@
                     </div>
                 </div>
             </div>
-
-
-
-
-
-
         @endif
 
         @if ($career?->block_four_title)
